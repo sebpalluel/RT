dbeebbfd3474827a9f1e1fb7a8c202b24e0e4071
sebpalluel <psebasti@student.42.fr>
2018-02-22 10:59:15 +0100

    updated ft_isfloat and ft_issize_t in libft

--- includes/rtv1_define.h
+++ includes/rtv1_define.h
@@ -6,7 +6,7 @@
 /*   By: psebasti <sebpalluel@free.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/12 20:31:26 by psebasti          #+#    #+#             */
-/*   Updated: 2018/02/21 17:20:42 by psebasti         ###   ########.fr       */
+/*   Updated: 2018/02/21 17:59:55 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 # define WIDTH			1280
 # define HEIGHT			720
 # define RATIO			WIDTH / HEIGHT
-# define FOV        45
+# define FOV			45
 # define DEG2RAD(x)    ((x * M_PI) / 180)
 # define MAX_SIZE		10000
 # define MAX_OBJ		20
