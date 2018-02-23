802a567f135d584e1ed48d4fbedf554bd22dc760
sebpalluel <psebasti@student.42.fr>
2018-02-23 17:48:27 +0100

    correct ft_isfloat

--- libft/srcs/ctype/ft_isfloat.c
+++ libft/srcs/ctype/ft_isfloat.c
@@ -6,7 +6,7 @@
 /*   By: psebasti <sebpalluel@free.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/21 17:16:00 by psebasti          #+#    #+#             */
-/*   Updated: 2018/02/23 17:28:02 by psebasti         ###   ########.fr       */
+/*   Updated: 2018/02/23 17:48:04 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,7 @@ static char		*ft_checksignerr(char *s)
 	return (s);
 }
 
-static t_bool	ft_checknumerror(char *s, t_bool dot, t_bool get_num, \
-		t_bool get_space)
+static t_bool	ft_checknumerror(char *s, t_bool dot, t_bool get_num)
 {
 	while (s && *s)
 	{
@@ -58,14 +57,12 @@ t_bool			ft_isfloat(char *s)
 {
 	t_bool		dot;
 	t_bool		get_num;
-	t_bool		get_space;
 
 	dot = FALSE;
 	get_num = FALSE;
-	get_space = FALSE;
 	if (!s || !*s)
 		return (ERROR);
 	if (!(s = ft_checksignerr(s)))
 		return (ERROR);
-	return (ft_checknumerror(s, dot, get_num, get_space));
+	return (ft_checknumerror(s, dot, get_num));
 }
