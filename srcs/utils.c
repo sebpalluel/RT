19fad6f9794236b239ba8ade5f734971b8cb3b65
sebpalluel <psebasti@student.42.fr>
2018-02-23 11:17:54 +0100

    implementation of ft_isfloat and ft_issize_t in parser worked

--- srcs/utils.c
+++ srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: psebasti <sebpalluel@free.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/23 15:59:10 by psebasti          #+#    #+#             */
-/*   Updated: 2018/02/20 11:42:09 by psebasti         ###   ########.fr       */
+/*   Updated: 2018/02/23 11:15:42 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ t_bool			ft_getvectfromenv(t_vec3 *vect, char *value)
 	char		**vec_tmp;
 
 	vec_tmp = ft_strsplit(value, ',');
-	if (!vec_tmp || !vec_tmp[0] || !vec_tmp[1] || !vec_tmp[2] || vec_tmp[3])
+	if (!vec_tmp || ft_isfloat(vec_tmp[0]) != OK || ft_isfloat(vec_tmp[1]) \
+			!= OK || ft_isfloat(vec_tmp[2]) != OK || vec_tmp[3])
 		return (ERROR);
 	vect->x = ft_atof(vec_tmp[0]);
 	vect->y = ft_atof(vec_tmp[1]);
@@ -31,8 +32,9 @@ t_bool			ft_getcolfromenv(t_col *col, char *value)
 	char		**col_tmp;
 
 	col_tmp = ft_strsplit(value, ',');
-	if (!col_tmp || !col_tmp[0] || !col_tmp[1] || !col_tmp[2] || !col_tmp[3] \
-			|| col_tmp[4])
+	if (!col_tmp || ft_isfloat(col_tmp[0]) == ERROR || ft_isfloat(col_tmp[1]) \
+			== ERROR || ft_isfloat(col_tmp[2]) == ERROR || \
+			ft_isfloat(col_tmp[3]) == ERROR || col_tmp[4])
 		return (ERROR);
 	col->r = ft_atof(col_tmp[0]);
 	col->g = ft_atof(col_tmp[1]);
@@ -45,13 +47,13 @@ t_bool			ft_getcolfromenv(t_col *col, char *value)
 t_bool			ft_getdoublefromenv(double *val, char *value)
 {
 	*val = ft_atof(value);
-	return (OK);
+	return (ft_isfloat(value));
 }
 
 t_bool			ft_getsize_tfromenv(size_t *val, char *value)
 {
 	*val = (size_t)ft_atoi(value);
-	return (OK);
+	return (ft_issize_t(value));
 }
 
 t_bool			ft_checkifallset(t_bool *flag, size_t numvar)
@@ -70,7 +72,6 @@ void			ft_put_pixel(t_setup *setup, int x, int y, int color)
 {
 	int			*tmp;
 	size_t		index;
-	//static t_bool one = OK;
 
 	if (y >= (int)SCN.height || x >= (int)SCN.width || x < 0 || y < 0)
 		return ;
