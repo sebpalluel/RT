5e10ab0e0558db89f546b7717945226d6c13dccb
sebpalluel <psebasti@student.42.fr>
2018-02-26 17:43:59 +0100

    commit before merge

--- srcs/mlx_process.c
+++ srcs/mlx_process.c
@@ -6,7 +6,7 @@
 /*   By: psebasti <sebpalluel@free.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/14 18:01:08 by psebasti          #+#    #+#             */
-/*   Updated: 2018/02/26 17:39:02 by psebasti         ###   ########.fr       */
+/*   Updated: 2018/02/26 17:42:10 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ int			ft_expose(t_setup *setup)
 		clock_gettime(CLOCK_REALTIME, &end);
 		delta_time = ( end.tv_sec - start.tv_sec ) \
 					 + ( end.tv_nsec - start.tv_nsec ) / 1E9;
-		printf( "%lf\n", delta_time);
+		printf( "%lf sec\n", delta_time);
 		setup->mode = STATE_STOP;
 	}
 	//if (!setup->ui)
