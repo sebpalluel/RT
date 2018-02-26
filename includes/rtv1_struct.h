c074418a3d3e733b9e1592307d3555b5a348827d
sebpalluel <psebasti@student.42.fr>
2018-02-26 14:00:06 +0100

    delete camtoworld in struct t_cam because not needed

--- includes/rtv1_struct.h
+++ includes/rtv1_struct.h
@@ -6,7 +6,7 @@
 /*   By: psebasti <sebpalluel@free.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/12 20:32:54 by psebasti          #+#    #+#             */
-/*   Updated: 2018/02/23 17:24:59 by psebasti         ###   ########.fr       */
+/*   Updated: 2018/02/26 13:58:59 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,6 @@ typedef struct		s_cam
 	t_vec3			rgt;
 	t_vec3			dwn;
 	t_vec3			look_at;
-	double			**camToWorld;
 	size_t			num_arg;
 }					t_cam;
 
