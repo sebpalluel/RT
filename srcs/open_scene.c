8f0389e8cef34d1ff8d733c44d8d3f07a4a0ff9e
sebpalluel <psebasti@student.42.fr>
2018-02-27 12:23:12 +0100

    norm open_scene

--- srcs/open_scene.c
+++ srcs/open_scene.c
@@ -6,7 +6,7 @@
 /*   By: psebasti <sebpalluel@free.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/29 17:20:12 by psebasti          #+#    #+#             */
-/*   Updated: 2018/02/21 15:11:38 by psebasti         ###   ########.fr       */
+/*   Updated: 2018/02/27 12:23:01 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,41 +21,25 @@ size_t			ft_envtosetup(t_setup *setup)
 
 	env = SCN.env;
 	validobjs = ft_validobjs();
-	//while (env) // can be used to see all the linked list for eventual debug
-	//{
-	//	if (env && ENVSTRUCT(env))
-	//	{
-	//		printf("name : %s\n", ENVSTRUCT(env)->name);
-	//		printf("value : %s\n", ENVSTRUCT(env)->value);
-	//	}
-	//	env = env->next;
-	//}
-	//env = SCN.env;
 	while (env && ENVSTRUCT(env))
 	{
 		i = -1;
-		flag = ERROR; // je part du principe que l'element est inconnu
-		while (env && ++i < NUM_OBJS) // NUM_OBJS nombre d'objets que l'on sait gerer
+		flag = ERROR;
+		while (env && ++i < NUM_OBJS)
 		{
-			if (ft_strcmp(ENVSTRUCT(env)->name, validobjs[i]) == 0) // ici permet de savoir si cet element est pris en charge
+			if (ft_strcmp(ENVSTRUCT(env)->name, validobjs[i]) == 0)
 			{
 
-				flag = OK; // dans ce cas la le chainon est valide
-				if (parse_obj()[i](&env) != OK) // ici on rentre dans la fonction de l'objet correspondant (par example pour name "sphere" on rentre dans la fonction ft_sphere
-					return (ERROR); // dans le cas ou cette structure est mal formatee (information qui manque etc)
+				flag = OK;
+				if (parse_obj()[i](&env) != OK)
+					return (ERROR);
 				if (env)
-					env = env->next; // si c'est ok on passe au chainon suivant qui devra correspondre a un objet qui qui est pris en charge (cam, light, sphere etc.)
+					env = env->next;
 			}
 		}
-		if (flag == ERROR) // ici dans le cas ou aucun objet correspondant
+		if (flag == ERROR)
 			return (ERROR);
 	}
-	//env = SCN.forms;
-	//while (env) // can be used to see all the linked list for eventual debug
-	//{
-	//	printf("parser form %p, type %d\n", FORM(env), FORM(env)->type);
-	//	env = env->next;
-	//}
 	return (OK);
 }
 
@@ -72,18 +56,6 @@ size_t			ft_select_scene(t_setup *setup, int scene)
 	else
 		return (ERROR);
 }
-
-//size_t			ft_alloc_objs(t_setup *setup) // alloue chaque objets
-//{
-//	//TODO if new objs adapt
-//	PLANE = (t_plane *)ft_memalloc(sizeof(t_plane) * MAX_OBJ);
-//	SPHERE = (t_sphere *)ft_memalloc(sizeof(t_sphere) * MAX_OBJ);
-//	CAM = (t_cam *)ft_memalloc(sizeof(t_cam) * MAX_OBJ);
-//	LIGHT = (t_light *)ft_memalloc(sizeof(t_light) * MAX_OBJ);
-//	if (PLANE == NULL || CAM == NULL || LIGHT == NULL || SPHERE == NULL)
-//		return (ERROR);
-//	return (OK); // a ce moment tout est alloue, setup->completement ready
-//}
 
 size_t			ft_init_new_scene(t_setup *setup)
 {
@@ -104,27 +76,24 @@ size_t			ft_open_scene(t_setup *setup)
 	file = NULL;
 	line = NULL;
 	if (ft_init_new_scene(setup) != OK || \
-			ft_open(&SCN.fd, O_RDONLY, O_APPEND) != OK) // permet de gerer cas d'erreur d'open, pas les droits etc
+			ft_open(&SCN.fd, O_RDONLY, O_APPEND) != OK)
 		return (setup->error = FILE_ERROR);
 	while (get_next_line(SCN.fd.fd, &line))
 	{
-		if (!line) // permet de gerer le cas d'erreur d'ouverture d'un dossier
+		if (!line)
 			return (setup->error = FILE_ERROR);
 		tmp = file;
 		file = ft_strjoin(tmp, line);
 		free(tmp);
 	}
-	// le fichier est bien stocke dans file et il faut le parser
 	if (!(SCN.env = ft_parse_scn(setup, file)) || ft_envtosetup(setup) != OK\
-			|| setup->error != OK) // ft_envlist retourne la list chainee peuplee, ft_envtosetup se charge du parsing et de la population des structures
+			|| setup->error != OK)
 		return (ERROR);
 	if (SCN.num_cam == 0)
 		return (setup->error = CAM_ERROR);
-	// appartir de la, on a le setup qui est entierement peuple et aucune erreur
-	//OBJDEF.objscount = ft_getobjscount(setup); // permet de savoir combien d'objet le raytracer va devoir traiter
 	if (!setup->num_scn)
 		setup->num_scn = 1;
 	mlx_put_image_to_window(setup->mlx_ptr, UI_WIN->win_ptr, UI_IMG->image, 0, 0);
-	setup->mode = STATE_DRAW; // on peut render !!
+	setup->mode = STATE_DRAW;
 	return (OK);
 }
