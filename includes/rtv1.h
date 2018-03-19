8f4f0522e18cf51293c6562e5f42777c3d95e19c
sebpalluel <psebasti@student.42.fr>
2018-03-19 12:17:03 +0100

    commit before merge branch AddingAlpha

--- includes/rtv1.h
+++ includes/rtv1.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rtv1.h                                           :+:      :+:    :+:   */
+/*   rtv1.h                                           :+:      :+:    :+:     */
 /*                                                    +:+ +:+         +:+     */
 /*   By: psebasti <sebpalluel@free.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/12 20:25:18 by psebasti          #+#    #+#             */
-/*   Updated: 2018/03/13 17:03:50 by psebasti         ###   ########.fr       */
+/*   Updated: 2018/03/14 11:58:11 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 # include "../includes/rtv1_global.h"
 # include <time.h>
 # include <sys/time.h>
-#include <SDL.h>
+# include "/Users/psebasti/Library/Frameworks/SDL2.framework/Headers/SDL.h"
 
 int			usage(int mode);
 int			ft_quit(t_setup *setup);
--- libft/srcs/gfx/ft_vec3addscale_r.c
+++ libft/srcs/gfx/ft_vec3addscale_r.c
@@ -6,7 +6,7 @@
 /*   By: psebasti <sebpalluel@free.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/27 16:40:09 by psebasti          #+#    #+#             */
-/*   Updated: 2018/02/27 16:41:24 by psebasti         ###   ########.fr       */
+/*   Updated: 2018/03/14 17:27:49 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 t_vec3	ft_vec3addscale_r(t_vec3 v, double a)
 {
-	v.x *= a;
-	v.y *= a;
-	v.z *= a;
+	v.x += a;
+	v.y += a;
+	v.z += a;
 	return (v);
 }
