99f35ae1cf6e4878442eef167e02e57e86c47419
sebpalluel <psebasti@student.42.fr>
2018-03-20 11:41:05 +0100

    second draft of work for cylindre, now will implement perlin data form mat

--- srcs/texture.c
+++ srcs/texture.c
@@ -2,20 +2,20 @@
 
 t_col get_px_color(SDL_PixelFormat *fmt, Uint32 pixel)
 {
-  t_col color;
-  Uint8 r;
-  Uint8 g;
-  Uint8 b;
-  Uint8 s;
+	t_col color;
+	Uint8 r;
+	Uint8 g;
+	Uint8 b;
+	Uint8 s;
 
-  SDL_GetRGBA(pixel, fmt, &r, &g, &b, &s);
-  color.r = r / 255.0;
-  color.g = g / 255.0;
-  color.b = b / 255.0;
-  color.s = s / 255.0;
-  //if (color.s != 0)
-    // printf("Pixel Color -> R: %f,  G: %f,  B: %f, A: %f\n", color.r, color.g, color.b, color.s);
-  return color;
+	SDL_GetRGBA(pixel, fmt, &r, &g, &b, &s);
+	color.r = r / 255.0;
+	color.g = g / 255.0;
+	color.b = b / 255.0;
+	color.s = s / 255.0;
+	//if (color.s != 0)
+	// printf("Pixel Color -> R: %f,  G: %f,  B: %f, A: %f\n", color.r, color.g, color.b, color.s);
+	return color;
 }
 
 /*
@@ -30,24 +30,24 @@ Uint32 getpixel(SDL_Surface *surface, int x, int y)
 	/* Here p is the address to the pixel we want to retrieve */
 	Uint8 *p = (Uint8 *)surface->pixels + y * surface->pitch + x * bpp;
 
-    switch(bpp) {
-    case 1:
-        return *p;
-        break;
-    case 2:
-        return *(Uint16 *)p;
-        break;
+	switch(bpp) {
+		case 1:
+			return *p;
+			break;
+		case 2:
+			return *(Uint16 *)p;
+			break;
 
-    case 3:
-        if(SDL_BYTEORDER == SDL_BIG_ENDIAN)
-            return p[0] << 16 | p[1] << 8 | p[2];
-        else
-            return p[0] | p[1] << 8 | p[2] << 16;
-        break;
+		case 3:
+			if(SDL_BYTEORDER == SDL_BIG_ENDIAN)
+				return p[0] << 16 | p[1] << 8 | p[2];
+			else
+				return p[0] | p[1] << 8 | p[2] << 16;
+			break;
 
-    case 4:
-        return *(Uint32 *)p;
-        break;
+		case 4:
+			return *(Uint32 *)p;
+			break;
 
 		default:
 			return 0;       /* shouldn't happen, but avoids warnings */
@@ -56,63 +56,63 @@ Uint32 getpixel(SDL_Surface *surface, int x, int y)
 
 void cpy_px_map(t_text *texture, SDL_Surface *img)
 {
-  Uint32 pixel;
-  int x;
-  int y;
+	Uint32 pixel;
+	int x;
+	int y;
 
-  (void)texture;
-  y = 0;
-  x = 0;
-  pixel = 0;
-  while (x < img->w)
-  {
-    while (y < img->h)
-    {
-      pixel = getpixel(img, x, y);
-      texture->map[x + y * img->w] = get_px_color(img->format, pixel);
-      y++;
-    }
-    y = 0;
-    x++;
-  }
+	(void)texture;
+	y = 0;
+	x = 0;
+	pixel = 0;
+	while (x < img->w)
+	{
+		while (y < img->h)
+		{
+			pixel = getpixel(img, x, y);
+			texture->map[x + y * img->w] = get_px_color(img->format, pixel);
+			y++;
+		}
+		y = 0;
+		x++;
+	}
 }
 /*
-potentiellement utile
-** SDL_PixelFormat* pixelFormat = img->format;
-** Uint32 pixelFormatEnum = pixelFormat->format;
-** read_img(img);
-** const char* surfacePixelFormatName = SDL_GetPixelFormatName(pixelFormatEnum);
-** SDL_Log("The surface's pixelformat is %s", surfacePixelFormatName);
-**
-** Check the bitdepth of the surface
-** printf("%d bits pxl\n", img->format->BitsPerPixel);
-*/
+   potentiellement utile
+ ** SDL_PixelFormat* pixelFormat = img->format;
+ ** Uint32 pixelFormatEnum = pixelFormat->format;
+ ** read_img(img);
+ ** const char* surfacePixelFormatName = SDL_GetPixelFormatName(pixelFormatEnum);
+ ** SDL_Log("The surface's pixelformat is %s", surfacePixelFormatName);
+ **
+ ** Check the bitdepth of the surface
+ ** printf("%d bits pxl\n", img->format->BitsPerPixel);
+ */
 t_text *get_texture_datas(char *path)
 {
-  SDL_Surface *img;
-  t_text  *texture;
+	SDL_Surface *img;
+	t_text  *texture;
 
-  img = NULL;
-  if (!(texture = malloc(sizeof(t_text))))
-    exit(1);
-  if (!(img = IMG_Load(path)))
-  {
-    printf("load failed\n");
-    free(texture);
-    exit (1);
-  }
-  else
-  {
-    texture->img_w = img->w;
-    texture->img_h = img->h;
-    if(!(texture->map = malloc(sizeof(t_col) * (img->w * img->h))))
-      exit(1);
-    SDL_LockSurface(img);
-    cpy_px_map(texture, img);
-    SDL_UnlockSurface(img);
-    SDL_FreeSurface(img);
-  }
-  return (texture);
+	img = NULL;
+	if (!(texture = malloc(sizeof(t_text))))
+		exit(1);
+	if (!(img = IMG_Load(path)))
+	{
+		printf("load failed\n");
+		free(texture);
+		exit (1);
+	}
+	else
+	{
+		texture->img_w = img->w;
+		texture->img_h = img->h;
+		if(!(texture->map = malloc(sizeof(t_col) * (img->w * img->h))))
+			exit(1);
+		SDL_LockSurface(img);
+		cpy_px_map(texture, img);
+		SDL_UnlockSurface(img);
+		SDL_FreeSurface(img);
+	}
+	return (texture);
 }
 
 t_mat		get_mat_at(t_vec3 hit, t_list *form, t_mat mat_obj)
@@ -122,18 +122,20 @@ t_mat		get_mat_at(t_vec3 hit, t_list *form, t_mat mat_obj)
 	t_col	prev_col;
 
 	hit_mat = mat_obj;
-	if (mat_obj.text > 0 && mat_obj.text <= NUM_TEXT)
+	if (FORM(form)->type < NUM_FORM - 1) // TODO bypass torus and moebius
 	{
-		text = get_st()->textures[mat_obj.text - 1];
-		uv_map()[FORM(form)->type - 1](hit, form, &hit_mat, text);
-	}
-	if (mat_obj.text > NUM_TEXT && mat_obj.text <= (NUM_TEXT + NUM_PROC))
-	{
-		prev_col = hit_mat.col;
-		hit = uv_map()[FORM(form)->type - 1](hit, form, &hit_mat, text);
-		//if (FORM(form)->type == PLN)
-		//	printf("x %f y %f\n", hit.x, hit.y);
-		hit_mat.col = ft_colinterpol(hit_mat.col, ft_col_r(0., 0., 0., 1.), effects()[mat_obj.text - NUM_TEXT - 1](hit));
+		if (mat_obj.text_m == M_TEX)
+		{
+			text = get_st()->textures[mat_obj.text_c.text_num];
+			uv_map()[FORM(form)->type - 1](hit, form, &hit_mat, text);
+		}
+		if (mat_obj.text_m == M_GEN && mat_obj.gen.mode < NUM_PROC)
+		{
+			prev_col = hit_mat.col;
+			hit = uv_map()[FORM(form)->type - 1](hit, form, &hit_mat, text);
+			hit_mat.col = ft_colinterpol(hit_mat.col, ft_col_r(0., 0., 0., 1.), \
+					effects()[mat_obj.gen.mode](hit));
+		}
 	}
 	return (hit_mat);
 }
--- srcs/uv_maps.c
+++ srcs/uv_maps.c
@@ -10,7 +10,7 @@ t_vec3 uv_map_sph(t_vec3 hit, t_list *form, t_mat *mat, t_text *text)
 	u = 0.5 + atan2(tmp.z, tmp.x) / (2 * M_PI);
 	tmp = ft_vec3sop_r(tmp, FORM(form)->sph.r, '/');
 	v = 0.5 - asin(tmp.y) / M_PI;
-	if (mat->text > 0 && mat->text <= NUM_TEXT)
+	if (mat->text_m == M_TEX && text)
 	{
 		fit_and_scale(&u, &v, text);
 		mat->col = text->map[(int)u + (int)v * text->img_w];
@@ -33,7 +33,7 @@ t_vec3 uv_map_pln(t_vec3 hit, t_list *form, t_mat *mat, t_text *text)
 	y_axe = ft_vec3normalize_r(ft_vec3vop_r(x_axe, FORM(form)->plan.nrml, 'c'));
 	u = ft_vec3dot(hit, x_axe);
 	v = ft_vec3dot(hit, y_axe);
-	if (mat->text > 0 && mat->text <= NUM_TEXT)
+	if (mat->text_m == M_TEX)
 	{
 		fit_and_scale(&u, &v, text);
 		mat->col = text->map[(int)u + (int)v * text->img_w];
@@ -51,7 +51,7 @@ t_vec3 uv_map_cyl(t_vec3 hit, t_list *form, t_mat *mat, t_text *text)
 	get_local_coords(hit, &coord, FORM(form)->cyl.pos, FORM(form)->cyl.dir);
 	u = atan2(coord.y, coord.x) / (2 * M_PI);
 	v = coord.z;
-	if (mat->text > 0 && mat->text <= NUM_TEXT)
+	if (mat->text_m == M_TEX && text)
 	{
 		fit_and_scale(&u, &v, text);
 		mat->col = text->map[(int)u + (int)v * text->img_w];
@@ -69,7 +69,7 @@ t_vec3 uv_map_cone(t_vec3 hit, t_list *form, t_mat *mat, t_text *text)
 	get_local_coords(hit, &coord, FORM(form)->cone.org, FORM(form)->cone.dir);
 	u = atan2(coord.y, coord.x) / (2 * M_PI);
 	v = coord.z;
-	if (mat->text > 0 && mat->text <= NUM_TEXT)
+	if (mat->text_m == M_TEX && text)
 	{
 		fit_and_scale(&u, &v, text);
 		mat->col = text->map[(int)u + (int)v * text->img_w];
