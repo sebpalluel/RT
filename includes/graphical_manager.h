bf13c177fd8325d040d5a563cbab82d22e7954d7
sebpalluel <psebasti@student.42.fr>
2018-03-29 17:11:41 +0200

    small modif header, wrong proto

--- includes/graphical_manager.h
+++ includes/graphical_manager.h
@@ -6,7 +6,7 @@
 /*   By: mbeilles <mbeilles@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 16:57:34 by mbeilles          #+#    #+#             */
-/*   Updated: 2018/03/29 17:07:42 by psebasti         ###   ########.fr       */
+/*   Updated: 2018/03/29 17:11:28 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,10 +29,10 @@ typedef enum			e_post_effect
 
 void					apply_post_effect(t_cam *cam, t_post_effect e);
 
-t_col					*ft_sepia(SDL_Surface *s);
-t_col					*ft_cel_shading(SDL_Surface *s);
-t_col					*ft_negative(SDL_Surface *s);
-t_col					*ft_blackandwhite(SDL_Surface *s);
-t_col					*ft_blur(SDL_Surface *s);
+t_col					*ft_sepia(SDL_Surface *s, int x, int y);
+t_col					*ft_cel_shading(SDL_Surface *s, int x, int y);
+t_col					*ft_negative(SDL_Surface *s, int x, int y);
+t_col					*ft_blackandwhite(SDL_Surface *s, int x, int y);
+t_col					*ft_blur(SDL_Surface *s, int x, int y);
 
 #endif
