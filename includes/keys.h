e9fdb4a94e3ab9db6970e5b6891973ec8c831b53
Njiall <njiall@njialls-imac.home>
2018-03-30 18:24:41 +0200

    keys handler fixes

--- includes/keys.h
+++ includes/keys.h
@@ -6,7 +6,7 @@
 /*   By: mbeilles <mbeilles@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/13 09:10:24 by mbeilles          #+#    #+#             */
-/*   Updated: 2018/03/30 18:22:11 by mbeilles         ###   ########.fr       */
+/*   Updated: 2018/03/30 18:24:36 by mbeilles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,23 +53,23 @@ typedef struct				s_key
 
 t_key						*init_keys(void);
 
-void						handle_keyboard(t_env *env);
+void						handle_keyboard();
 
 uint32_t					is_combinason_pressed(t_key k, const uint8_t *keys);
 SDL_Scancode				*create_codes(int32_t codes, ...);
 t_key						create_key(uint32_t keys, SDL_Scancode *codes
 										, t_key_func f);
 
-void						exit_rt(t_env *env);
-void						toggle_fullscreen(t_env *env);
-void						perspepective_up(t_env *env);
-void						perspepective_down(t_env *env);
-void						rpp_up(t_env *env);
-void						rpp_down(t_env *env);
-void						expo_up(t_env *env);
-void						expo_down(t_env *env);
-void						threshold_up(t_env *env);
-void						threshold_down(t_env *env);
+void						exit_rt();
+void						toggle_fullscreen();
+void						perspepective_up();
+void						perspepective_down();
+void						rpp_up();
+void						rpp_down();
+void						expo_up();
+void						expo_down();
+void						threshold_up();
+void						threshold_down();
 
 /*
 ** =============================================================================
